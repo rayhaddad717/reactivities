d065a66fc551743b6b6bcdc2c4172e8ca9da65d3
Ray Haddad <rayhaddad@globalistic.tech>
2023-08-05 10:11:05 +0300

    refactored add application services

--- API/Program.cs
+++ API/Program.cs
@@ -1,32 +1,13 @@
 using Persistence;
 using Microsoft.EntityFrameworkCore;
-using Application.Activities;
-using MediatR;
-using Application.Core;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(opt => {
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-
-builder.Services.AddCors(opt =>
-{
-    opt.AddPolicy("CorsPolicy",policy =>
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    }
-    );
-}
-);
-builder.Services.AddMediatR(typeof(List.Handler));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -59,6 +40,6 @@ try
 catch (Exception ex)
 {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex,"An error has occured during migration");
+    logger.LogError(ex, "An error has occured during migration");
 }
 app.Run();

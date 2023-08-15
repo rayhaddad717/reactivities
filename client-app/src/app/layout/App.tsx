7584708d7a8c6f5e5f8e6bd45432ee4705591595
Ray Haddad <rayghaddad@gmail.com>
2023-08-15 10:41:10 +0300

    local crud for activities

---
+++ client-app/src/app/layout/App.tsx
@@ -0,0 +1,79 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Container } from "semantic-ui-react";
+import { Activity } from "../models/activity";
+import NavBar from "./NavBar";
+import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
+import { v4 as uuid } from "uuid";
+function App() {
+  const [activities, setActivities] = useState<Activity[]>([]);
+  const [selectedActivity, setSelectedActivity] = useState<
+    Activity | undefined
+  >(undefined);
+  const [editMode, setEditMode] = useState(false);
+  useEffect(() => {
+    function init() {
+      axios
+        .get<Activity[]>("http://localhost:5000/api/activities", {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImJvYiIsIm5hbWVpZCI6ImJiMDEyNmEwLTk3ZjAtNGU5MC04ZjRlLWUyN2EzOTNkNzYzYiIsImVtYWlsIjoiYm9iQHRlc3QuY29tIiwibmJmIjoxNjkxNTAwMTAwLCJleHAiOjE2OTIxMDQ5MDAsImlhdCI6MTY5MTUwMDEwMH0.d_dX55vdWCB_vsckpS72iWohH8pVCemLuPq2b6vTE2vCJybx6CYgcWz87gr9IL_3ddyQarDB3g-Ldu7qi1kZ7Q",
+          },
+        })
+        .then((response) => {
+          setActivities(response.data);
+        })
+        .catch(console.error);
+    }
+    init();
+  }, []);
+  function handleSelectActivity(id: string) {
+    setSelectedActivity(activities.find((x) => x.id === id));
+  }
+  function handleCancelSelectActivity() {
+    setSelectedActivity(undefined);
+  }
+  function handleFormOpen(id?: string) {
+    id ? handleSelectActivity(id) : handleCancelSelectActivity();
+    setEditMode(true);
+  }
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleCreateOrEditActivity(activity: Activity) {
+    activity.id
+      ? setActivities([
+          ...activities.filter((x) => x.id !== activity.id),
+          activity,
+        ])
+      : setActivities([...activities, { ...activity, id: uuid() }]);
+    setEditMode(false);
+    setSelectedActivity(activity);
+  }
+
+  function handleDeleteActivity(id: string) {
+    setActivities([...activities.filter((x) => x.id !== id)]);
+  }
+
+  return (
+    <>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: "7em" }}>
+        <ActivityDashboard
+          activities={activities}
+          selectedActivity={selectedActivity}
+          cancelSelectActivity={handleCancelSelectActivity}
+          selectActivity={handleSelectActivity}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditActivity}
+          deleteActivity={handleDeleteActivity}
+        />
+      </Container>
+    </>
+  );
+}
+
+export default App;
